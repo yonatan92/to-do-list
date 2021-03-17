b51fe4a6ca011147d9eba4b0a065aa586707724a
yonatanberns <yonatan.bernstein5@gmail.com>
2021-03-17 12:56:51 +0200

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('inittt');
+console.log('init stage ..');
+// alert('newnewnew');
+
+localStorage.setItem('itm1', 'homework');
+localStorage.setItem('itm2', 'going to gym');
+
+
+let itm1 = localStorage.getItem('itm1');
+console.log('my itm: ', itm1);
+
+var btn = document.getElementById('submit');
+btn.addEventListener('click', func);
+
+function func() {
+    console.log(document.getElementById("number").value)
+}
+
+
+
+document.querySelector('.test').textContent = itm1;
